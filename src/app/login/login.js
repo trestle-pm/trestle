2a68895170e5ce2b9d440d452f36f981cb2dbc48
nathan <codesuffle@gmail.com>
2013-08-21 20:29:13 -0500

    Add the ability for users to only stay logged in for a session

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -7,22 +7,24 @@ angular.module( 'GitKan.login', [
 .config(function config( $stateProvider ) {
   $stateProvider.state( 'login', {
     url: '/login',
-     controller: 'LoginCtrl',
+     controller:  'LoginCtrl',
      templateUrl: 'login/login.tpl.html'
   });
 })
 
 .controller( 'LoginCtrl', function HomeController($scope, $http, $location, gh) {
-   // Always start with a blonk username/password
-   $scope.username  = '';
-   $scope.passsword = '';
+   // Always start with a blonk username/password and remember turned off
+   $scope.username   = '';
+   $scope.passsword  = '';
+   $scope.rememberMe = false;
 
    $scope.attemptLogin = function() {
       var user = $scope.username, pass = $scope.password;
 
       // Make sure the user entered something
       if (!user || !pass) {
-         console.log('dude you must enter something');
+         console.log('Dude you must enter something');
+         // XXX look at basic field validation
          return;
       }
 
@@ -48,11 +50,16 @@ angular.module( 'GitKan.login', [
          // XXX
 
          // Cache the token so that we can use it later
-         // XXX Look at a checkbox and store to session storage if the user
-         // does not say to remember them.
          if (auth) {
-            // XXX pass this to `gh` somehow
-            window.localStorage.setItem('gh-token', auth.token);
+            // Pass the token off to the `gh` service
+            // - Tell the github service how long to hold the authentication
+            gh.setAccessToken(auth.token,
+                              $scope.rememberMe ? 'local' : 'session');
+
+            // Yeah, for successful auth so bounce the user to next page
+            // - If the next page was supplied then use that otherwise
+            //   bounce the user to the repo list by default
+            $location.path('/repos');
          }
       });
 
