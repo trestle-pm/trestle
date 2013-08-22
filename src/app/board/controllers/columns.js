442ad77cbd43b189ba80f8696fff59ccb2842ee5
nathan <codesuffle@gmail.com>
2013-08-21 22:36:09 -0500

    Add the ability to drag an issue/pull out of github directly into the board

--- src/app/board/controllers/columns.js
+++ src/app/board/controllers/columns.js
@@ -1,6 +1,8 @@
 angular.module('GitKan.board')
 
 .controller('ColumnsCtrl', function($scope, $stateParams, gh) {
+   // Grab the configuration file for this repo so that we know
+   // the column names
    gh.getFile($stateParams.owner, $stateParams.repo, '.huboard')
       .then(function(contents) {
          var config = JSON.parse(contents);
@@ -22,6 +24,56 @@ angular.module('GitKan.board')
          });
          return _.contains(labels, column);
       };
+   };
+
+   /**
+    Called when an issue starts being dragged so to set the mime-type/data for
+    the drag event.  This data is used by other systems to determine if
+    they can handle the event.
+    */
+   $scope.getDragData = function(issue, $event) {
+      // Return three of the common ways to pass urls
+      // Ideally, this will allow us to interoperate with other systems.
+      return {
+         'URL':           issue.html_url,
+         'text/uri-list': issue.html_url,
+         'text/plain':    issue.html_url
+      };
+   };
+
+   /**
+    Called when a drag is over a column.  The goal is to check the mime-type/data
+    for the drag event and return if the column can handle the data.
+
+    In this case we handle:
+     * issue urls
+     * pull urls
+    */
+   $scope.canHandleDrop = function($event) {
+      // See if there is a github URL
+      var url = _.find(['URL', 'text/uri-list', 'text/plain'], function(mimeType) {
+         var data = $event.dataTransfer.getData(mimeType);
+         var grabber = /https?:\/\/github\.com\/(.*)\/(.*)\/(pull|issues)\/([0-9]{1,5})/;
+
+         var matches = grabber.exec(data);
+         if (matches) {
+            console.log('is_gh');
+            var owner  = matches[1],
+                repo   = matches[2];
+
+            if (owner === $stateParams.owner && repo === $stateParams.repo) {
+               return data;
+            }
+         }
+
+         return false;
+      });
+
+      return url ? true : false;
+   };
+
+   $scope.onDrop = function() {
+      console.log('drop');
    };
 })
 
--- src/common/draggable/draggable.js
+++ src/common/draggable/draggable.js
@@ -1,28 +1,99 @@
 angular.module('draggable', [])
 
-.directive('ngDraggable', function() {
+.directive('ngDraggable', function($parse) {
    return {
-      link: function(scope, iElm, iAttrs, controller) {
-         iElm.prop('draggable', true);
-         console.log(iElm);
-         iElm[0].ondragstart = function(evt) {
-            evt.dataTransfer.setData('Text', 'blah');
-         };
+      link: function(scope, element, attrs, controller) {
+         // Mark the item as draggable
+         element.prop('draggable', true);
 
-         iElm[0].ondrop = function(evt) {
-            evt.preventDefault();
-            console.log('adf');
-         };
+         // When the drag starts fill in the drag data
+         // evt.dataTransfer.setData('text/', 'blah');
+         var func = $parse(attrs['ngDraggable']);
+         element.bind('dragstart', function(event) {
+            // Run the function inside a $scope.apply block so that if the
+            // scope changes the scope will be reevaluated.
+            scope.$apply(function() {
+               // See what types of data the drag should support
+               var types = func(scope, {$event: event});
+
+
+               // Add the types to the event so that they follow through the
+               // dragenter and drop events.
+               _.map(types, function(value, type) {
+                  event.dataTransfer.setData(type, value);
+               });
+            });
+         });
       }
    };
 })
 
-.directive('ngDropzone', function() {
+.directive('ngDropzone', function($parse) {
    return {
-      link: function(scope, iElm, iAttrs, controller) {
-         iElm[0].ondragenter = function(evt) {
-            console.log(evt.dataTransfer.getData('Text'));
-         };
+      link: function(scope, element, attrs, controller) {
+         var drag_type = 'move'; // Hardcoded for now since that is all I need
+
+         var func = $parse(attrs['ngDropzone']);
+         element.bind('dragenter', function(event) {
+            // Defualt to drop not allowed
+            var allow_drop = true;
+
+            scope.$apply(function() {
+               allow_drop = func(scope, {$event: event});
+            });
+
+            if (allow_drop) {
+               event.dataTransfer.dropEffect = drag_type;
+
+               // The event will continue propogating since we do not care
+               // about the drop
+               event.preventDefault();
+            }
+
+            return !allow_drop;
+         });
+
+         element.bind('dragover', function(event) {
+            // Defualt to drop not allowed
+            var allow_drop = false;
+
+            scope.$apply(function() {
+               allow_drop = func(scope, {$event: event});
+            });
+
+            if (allow_drop) {
+               event.dataTransfer.dropEffect = drag_type;
+
+
+               // The event will continue propogating since we do not care
+               // about the drop
+               if (allow_drop) {
+                  event.preventDefault();
+               }
+            }
+
+            return !allow_drop;
+         });
+
+         var doDropfunc = $parse(attrs['handledrop']);
+         element.bind('drop', function(event) {
+            var handled = false;
+
+            scope.$apply(function() {
+               var handled = doDropfunc(scope, {$event: event});
+               // Allow the drop func to forget about the return value unless
+               // they need to cancel the drop.
+               // This is OK since the drop would not happen if the
+               // dragenter/over methods had not succeeded.
+               handled = (handled !== false);
+               console.log('drop', handled);
+            });
+
+            if (handled) {
+               event.preventDefault();
+            }
+            return !handled;
+         });
       }
    };
 })
