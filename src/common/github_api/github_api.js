9463a75010e88f80619666730325028af45b2e5a
nathan <codesuffle@gmail.com>
2013-08-21 20:23:16 -0500

    Allow passing the token to the gh service.  Also, allow the gh service to fail if the token is not set

--- src/common/github_api/github_api.js
+++ src/common/github_api/github_api.js
@@ -2,7 +2,33 @@ angular.module('github.api', [])
 
 .service('gh', ['$http', '$rootScope', '$q', function ($http, $rootScope, $q) {
    var gh_api = "https://api.github.com/",
-       token  = window.localStorage.getItem('gh-token');
+
+       token_storage_key = 'gh-token',
+       token;
+
+   function getAccessToken() {
+      // If the token is not set then try local storage
+      if (!token && window.localStorage) {
+         token = window.localStorage.getItem(token_storage_key);
+      }
+
+      // If the token is still not set try the session storage
+      if (!token && window.sessionStorage) {
+         token = window.sessionStorage.getItem(token_storage_key);
+      }
+
+      return token;
+   }
+
+   function assert_ready() {
+      // Try to get the access token
+      getAccessToken();
+
+      // Raise an error if the token could not be found
+      if (!token) {
+         throw new Error('Unable to find GitHub API access token');
+      }
+   }
 
    /**
     Helper function to convert GitHub's multiple string base 64 encoding into
@@ -37,28 +63,47 @@ angular.module('github.api', [])
       return d.promise;
    }
 
+   this.setAccessToken = function(newToken, storageRule) {
+      // Update our internal reference to the token
+      token = newToken;
+
+      // Set the value into storage if asked to
+      if (storageRule == 'local' && window.localStorage) {
+         window.localStorage.setItem(token_storage_key, newToken);
+      }
+      else if (storageRule == 'session' && window.sessionStorage) {
+         window.localStorage.setItem(token_storage_key, newToken);
+      }
+   };
+
    this.listRepoUsers = function(owner, repo) {
+      assert_ready();
       return ghGet("repos/"+owner+'/'+repo+"/collaborators");
    };
 
    this.listRepoIssues = function(owner, repo) {
+      assert_ready();
       // XXX look at how to get more then 100 issues for users that might hit that
       return ghGet("repos/"+owner+'/'+repo+"/issues", {'page': 1, 'per_page': 100});
    };
 
    this.listRepos = function() {
+      assert_ready();
       return ghGet("user/repos");
    };
 
    this.listOrgs = function() {
+      assert_ready();
       return ghGet('user/orgs');
    };
 
    this.listOrgRepos = function(org) {
+      assert_ready();
       return ghGet('orgs/'+org+'/repos');
    };
 
    this.listAllOrgRepos = function() {
+      assert_ready();
       var me = this;
 
       var d = $q.defer();
@@ -90,6 +135,7 @@ angular.module('github.api', [])
    };
 
    this.listAllRepos = function() {
+      assert_ready();
       var me = this;
 
       return $q.all([
@@ -109,6 +155,7 @@ angular.module('github.api', [])
     Downloads a file from Github and returns the file contents as a string.
     */
    this.getFile = function(owner, repo, path) {
+      assert_ready();
       var d = $q.defer();
 
       var p = ghGet("repos/"+owner+'/'+repo+"/contents/"+path);
