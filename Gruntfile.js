d889ae4761e96b3f5229781f2ca557a20cc5cc8f
nathan <codesuffle@gmail.com>
2013-08-28 13:31:42 -0500

    Rename delta task back to watch since the goal can be accomplished in the future in a better way

--- Gruntfile.js
+++ Gruntfile.js
@@ -384,7 +384,7 @@ module.exports = function ( grunt ) {
        *
        * But we don't need the same thing to happen for all the files.
        */
-      delta: {
+      watch: {
          /**
           * By default, we want the Live Reload to work for all tasks; this is
           * overridden in some tasks (like this file) where browser resources are
@@ -473,16 +473,6 @@ module.exports = function ( grunt ) {
    };
 
    grunt.initConfig( grunt.util._.extend( taskConfig, userConfig ) );
-
-   /**
-    * In order to make it safe to just compile or copy *only* what was changed,
-    * we need to ensure we are starting from a clean, fresh build. So we rename
-    * the `watch` task to `delta` (that's why the configuration var above is
-    * `delta`) and then add a new task called `watch` that does a clean build
-    * before watching for changes.
-    */
-   grunt.renameTask( 'watch', 'delta' );
-   grunt.registerTask( 'watch', [ 'build', 'karma:unit', 'delta' ] );
 
    /**
     * The default task is to build and compile.
