be4958b88202bd302505c05eaa03ee801ee7c00d
nathan <codesuffle@gmail.com>
2013-09-08 13:01:38 -0500

    feature: Centeralize querying and updating of issues.

--- src/app/board/columns/columns.js
+++ src/app/board/columns/columns.js
@@ -1,16 +1,64 @@
 angular.module('Trestle.board')
 
-.controller('ColumnsCtrl', function($scope, trRepoModel) {
-   this.hasLabel = function(column) {
-   this.showBacklog = false;
-      return function(issue) {
-         var labels = _.map(issue.labels, function(lbl) {
-            return _.last(lbl.name.split('-')).trim();
-         });
-         return _.contains(labels, column);
-      };
-   };
+/**
+ @ngdoc service
+ @name Trestle.board.issueHelpers
 
+ @description
+ Simple service to hold methods helpful to working with issues.
+
+ XXX this could be moved to the Restangular stub
+ */
+.service('issueHelpers', function() {
+   this.getIssueLabels = function(issue) {
+      return _.map(issue.labels, function(labelObj) {
+         return labelObj.name;
+      });
+   };
+})
+
+/**
+ @ngdoc filter
+ @name  Trestle.board.issuesWithLabel
+
+ @description
+ Takes an array of GitHub issues and filters it down to only issues with the
+ specified labelName
+
+ @param {String} labelName The textual name of a label all issues must have to
+        be returned by this filter.
+ @returns {Array} Set of issues with the label
+ */
+.filter('issuesWithLabel', function(issueHelpers) {
+   return function(issues, labelName) {
+      if (!labelName) { throw new Error('labelName must be set'); }
+
+      return _.filter(issues, function(issue) {
+         return _.contains(issueHelpers.getIssueLabels(issue), labelName);
+      });
+   };
+})
+
+/**
+ @ngdoc filter
+ @name  Trestle.board.issuesInBacklog
+
+ @description
+ Takes an array of GitHub issues and filters it down to only issues that do not
+ have a label used for a column.
+
+ @returns {Array} Set of issues that are not part of a column
+ */
+.filter('issuesInBacklog', function(issueHelpers, trRepoModel) {
+   return function(issues) {
+      var columns = trRepoModel.config.columns;
+      return _.filter(issues, function(issue) {
+         return _.intersection(issueHelpers.getIssueLabels(issue), columns).length === 0;
+      });
+   };
+})
+
+.controller('ColumnsCtrl', function($scope, trRepoModel) {
    this.getColumnWidth = function() {
       var config      = trRepoModel.config,
           num_columns = config ? config.columns.length : 0;
