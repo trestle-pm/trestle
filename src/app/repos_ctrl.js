4a8ba35a93d47fdbbac09bf5f3a27d1c5e25b1be
nathan <codesuffle@gmail.com>
2013-09-12 09:32:43 -0500

    feature: Add more advanced filtering

--- src/app/repos_ctrl.js
+++ src/app/repos_ctrl.js
@@ -1,7 +1,7 @@
 angular.module('Trestle')
 
 .controller( 'ReposCtrl', function($scope, $stateParams, $location, gh, auth,
-                                   trRepoModel, trIssueFilters) {
+                                   trRepoModel, trIssueFilters, trSessionModel) {
    $scope.$id = "ResposCtrl_" + $scope.$id;
 
    this.init = function() {
@@ -10,6 +10,12 @@ angular.module('Trestle')
 
       // Expose the filters
       $scope.issueFilters = trIssueFilters;
+
+      $scope.sessionModel = trSessionModel;
+
+      // Get the users details (avatar, name, ...)
+      // - This a `Promise` that when resolved will update the toolbar
+      $scope.sessionModel.user = gh.getUserDetails();
    };
 })
 
